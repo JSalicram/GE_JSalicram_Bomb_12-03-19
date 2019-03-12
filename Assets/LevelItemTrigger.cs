b1cc8d298a3cbd8d7e68d0d747c4db7f2ccfdc34
JSalicram <JSalicram@users.github.com>
2019-03-12 14:47:15 +0000

    trigger made

---
+++ Assets/LevelItemTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelItemTrigger : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D otherColliderInCollision)
+    {
+        Debug.Log(otherColliderInCollision + " entered the trigger of " + this.name);
+
+        LevelManager.instance.OnLevelItemTriggerEnter(otherColliderInCollision, this);
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * The LevelManager from the Week 6 Lecture
+ */
+public class LevelManager : MonoBehaviour {
+
+    public static LevelManager instance;
+
+    private void Awake()
+    {
+        // set the instance property/variable to this object
+        instance = this;
+    }
+
+
+    public void OnLevelItemTriggerEnter(Collider2D otherColliderInCollision, LevelItemTrigger itemInCollision)
+    {
+        if(otherColliderInCollision.name == "Character" && itemInCollision.name == "Bomb")
+        {
+            Debug.Log("triggered");
+            Explode();
+        }
+    }
+
+    public void Explode()
+    {
+
+    }
+}
