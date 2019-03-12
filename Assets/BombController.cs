38f340aca6c1442f5ac3230cb7b737906d897017
JSalicram <JSalicram@users.github.com>
2019-03-12 15:51:22 +0000

    Attempt for array

--- Assets/BombController.cs
+++ Assets/BombController.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class BombController : MonoBehaviour {
     private GameObject Bomb;
     float shake = 0.1f;
+    private Vector2 bombPos;
+    private float bombRadius;
+
+    public Collider2D[] objectsInZone;
 
     private void Start()
     {
         Bomb = gameObject;
+        bombPos = this.transform.position;
+        //bombRadius = GetComponent<Collider2D>();
     }
 
     public IEnumerator ExplodeCoroutine()
     {
-        float rndmTime = 50f;
+        float rndmTime = Random.Range(20, 50);
         //random number between 20 and 50
 
         for (float f = rndmTime; f >= 0; f -= 1f)
@@ -25,4 +31,16 @@ public class BombController : MonoBehaviour {
 
         LevelManager.instance.Explode();
     }
+
+    /*
+    Collider2D[] collidersInBombRadius = Physics2D.OverlapCircleAll(Transform.position, )
+    {
+        for(int i = 0; i<collidersInBombRadius, i++)
+        {
+            if(collidersInBombRadius[i] != this.gameObject)
+            {
+                Destroy(colliderInBombRadius[i].gameobject);
+            }
+        }
+    }*/
 }
